e837e295a0f1d2e38444058c8abba66ea49bc40e
Douglas Almeida <douglas.alu.lmb@gmail.com>
2020-08-29 11:10:46 -0300

    Adding content

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 import {
   SafeAreaView,
@@ -9,51 +9,79 @@ import {
   StyleSheet,
   TouchableOpacity,
 } from "react-native";
+import api from "./services/api";
+  
+  export default function App() {
+    const [ repositories, setRepositories ] = useState([]);
+  
+    useEffect(() => {
+      async function getDatas() {
+        const response = await api.get('/repositories');
+        setRepositories(response.data);
+      }
+      getDatas();
+    }, [])
 
-export default function App() {
-  async function handleLikeRepository(id) {
-    // Implement "Like Repository" functionality
+    async function handleLikeRepository(id) {
+      const response = await api.post(`/repositories/${id}/like`);
+      
+      const likedUpdate = response.data;
+
+      const repositoryUpdated = repositories.map(repository => {
+
+        if (repository.id === id) {
+          return likedUpdate;
+        } else {
+          return repository;
+        }
+      })
+      
+      setRepositories(repositoryUpdated);
+    }
+  
+    return (
+      <>
+        <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
+        
+        <SafeAreaView style={styles.container}>
+        <FlatList 
+              data={repositories}
+              keyExtractor={repository => repository.id}
+              renderItem={({ item: repository}) => (
+                <View style={styles.repositoryContainer}>
+                  <Text style={styles.repository}>{repository.title}</Text>
+                    
+                  <View style={styles.techsContainer}>
+                    {repository.techs.map(tech => (
+                      <Text key={tech} style={styles.tech}>
+                        {tech}                                                                            
+                      </Text>
+                    ))}
+                  </View>
+        
+                  <View style={styles.likesContainer}>
+                    <Text
+                      style={styles.likeText}
+                      testID={`repository-likes-${repository.id}`}
+                    >
+                      {repository.likes} curtidas
+                    </Text>
+                  </View>
+                    
+                  <TouchableOpacity
+                    style={styles.button}
+                    onPress={() => handleLikeRepository(repository.id)}
+                    testID={`like-button-${repository.id}`}
+                  >
+                    <Text style={styles.buttonText}>Curtir</Text>
+                  </TouchableOpacity>
+                </View>
+          )}
+         />
+        </SafeAreaView>
+      </>
+    );
   }
-
-  return (
-    <>
-      <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
-      <SafeAreaView style={styles.container}>
-        <View style={styles.repositoryContainer}>
-          <Text style={styles.repository}>Repository 1</Text>
-
-          <View style={styles.techsContainer}>
-            <Text style={styles.tech}>
-              ReactJS
-            </Text>
-            <Text style={styles.tech}>
-              Node.js
-            </Text>
-          </View>
-
-          <View style={styles.likesContainer}>
-            <Text
-              style={styles.likeText}
-              // Remember to replace "1" below with repository ID: {`repository-likes-${repository.id}`}
-              testID={`repository-likes-1`}
-            >
-              3 curtidas
-            </Text>
-          </View>
-
-          <TouchableOpacity
-            style={styles.button}
-            onPress={() => handleLikeRepository(1)}
-            // Remember to replace "1" below with repository ID: {`like-button-${repository.id}`}
-            testID={`like-button-1`}
-          >
-            <Text style={styles.buttonText}>Curtir</Text>
-          </TouchableOpacity>
-        </View>
-      </SafeAreaView>
-    </>
-  );
-}
 
 const styles = StyleSheet.create({
   container: {
